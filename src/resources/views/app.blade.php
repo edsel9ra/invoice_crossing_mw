afb86508ac24360559297c817a8c34e75604222c
Edson Ramos <edramosc@gmail.com>
2026-06-12 13:01:44 -0500

    Update application title in app.blade.php

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title inertia>{{ config('app.name', 'Invoice Crossing') }}</title>
+    <title inertia>{{ config('app.name', 'Concurso Mister Wings') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
